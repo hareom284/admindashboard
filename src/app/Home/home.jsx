c14faa2a1b968882e07007a47cfa9a660b53f44f
hareom284 <hareom284@gmail.com>
2023-05-30 22:33:01 +0630

    admin dashboard

--- src/app/Home/home.jsx
+++ src/app/Home/home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./home.scss";
 import  Slidebar from '../../components/slidebar';
-
+import Header from './Header/header';
 export default function home() {
   return (
     <div className='container'>
       <Slidebar/>
       <div className='content'>
-        <h1>this is content</h1>
+        <Header/> 
       </div>
     </div>
   )
